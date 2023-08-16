be4d7eb0b0afda7ce2306320ff907449631e87f7
Nickolaus D. Saint <nick@globus.org>
2023-08-16 09:02:25 -0700

    feat: Require login to the site

--- data_facility_enrollment_demo/views.py
+++ data_facility_enrollment_demo/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+
 from data_facility_enrollment_demo.arc_api import ARCClient
 
 from data_facility_enrollment_demo.gcs import lookup_gcs_stuff, create_acl
 from data_facility_enrollment_demo.search import create_search_record
 
 
+def index(request):
+
+    return render(request, "index.html", {})
+
+
+@login_required
 def onboarding(request):
     arc_client = ARCClient(request.user)
     arc = {
@@ -23,6 +31,7 @@ def onboarding(request):
     return render(request, "onboarding.html", context)
 
 
+@login_required
 def onboarding_complete(request):
     context = {}
     return render(request, "onboarding-complete.html", context)
