0490b057fc160fe38667b2bf87b542bcdd4e22bb
Nickolaus D. Saint <nick@globus.org>
2023-08-16 09:02:36 -0700

    feat: Add index page

--- data_facility_enrollment_demo/urls.py
+++ data_facility_enrollment_demo/urls.py
@@ -3,7 +3,8 @@ from django.urls import include, path
 from data_facility_enrollment_demo import views
 
 urlpatterns = [
-    path("", views.onboarding, name="onboarding"),
+    path("", views.index, name="index"),
+    path("onboarding/", views.onboarding, name="onboarding"),
     path("thanks/", views.onboarding_complete, name="onboarding-complete"),
     path("admin/", admin.site.urls),
     path("", include("globus_portal_framework.urls")),
