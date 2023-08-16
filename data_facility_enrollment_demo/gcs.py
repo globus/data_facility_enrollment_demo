efcc23c1fe986406d357e61e525d955f82a22896
David Kelly <davidkelly999@gmail.com>
2023-08-16 12:15:17 -0400

    Function for setting ACL on a collection

--- data_facility_enrollment_demo/gcs.py
+++ data_facility_enrollment_demo/gcs.py
@@ -1,5 +1,5 @@
 import logging
-from globus_sdk import TransferClient
+from globus_sdk import TransferClient, GlobusAPIError
 from django.contrib.auth.models import User
 
 log = logging.getLogger(__name__)
@@ -14,6 +14,34 @@ def lookup_gcs_stuff(user: User):
     return {}
 
 
-def create_acl(user: User):
+def create_acl(user: User, identity_id: str, endpoint_id: str, path: str, permissions: str):
+    """ Create an ACL
+
+    Keyword arguments:
+        user -- Django User object
+        identity_id -- Identity UUID for ACL rule
+        endpoint_id -- Endpoint/collection UUID
+        path -- Filesystem path (most end with a '/')
+        permissions -- 'r' for read only, 'rw' for read-write
+    """
     transfer_client: TransferClient = load_transfer_client(user)
     log.info('Created GCS ACL')
+
+    # ACL paths must end with slash
+    if path[-1] != "/":
+        path = path + "/"
+
+    rule_data = {
+        "DATA_TYPE": "access",
+        "principal_type": "identity",
+        "principal": identity_id,
+        "path": path,
+        "permissions": permissions,
+    }
+
+    try:
+        result = transfer_client.add_endpoint_acl_rule(endpoint_id, rule_data)
+        rule_id = result["access_id"]
+        return rule_id
+    except GlobusAPIError as e:
+        raise e.message
